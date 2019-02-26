47a1d70a7913840f889bbd276518b1d4e1b76bc6
Alex Whitworth <alexWhitworth@users.noreply.github.com>
2019-02-26 14:00:40 -0800

    Update install_packages.R
    updated packages to install

--- install_packages.R
+++ install_packages.R
@@ -1,21 +1,18 @@
-install.packages(c("lme4", "devtools", "merTools",  "randomForest", "Rcpp", "dplyr", "data.table", 
-                   "ggplot2", "lubridate", "ggvis", "quantmod", "zoo", "xts", "nnet", "HLMdiag", 
-                   "roxygen2", "foreign", "leaflet", "magrittr", "htmlwidgets", "shinydashboard",
-                   "stringr", "car", "shiny", "xtable", "testthat", "tidyr", "scales",
-                   "profr", "party", "kernlab", "e1071", "mclust", "reshape2", "testthat",
-                   "microbenchmark", "clustvarsel", "Rmixmod", "RColorBrewer", "RCurl",
-                   "gtools", "forecast", "RcppArmadillo", "RcppEigen", "rsconnect",
-                   "acs", "XML", "scrapeR", "xgboost"), 
-                 dependencies = c("Depends", "Imports"))
+install.packages(c(
+    "lme4", "devtools", "merTools",  "randomForest", "Rcpp", "dplyr", "data.table", 
+    "ggplot2", "lubridate", "ggvis", "quantmod", "zoo", "xts", "nnet", "HLMdiag", 
+    "roxygen2", "foreign", "leaflet", "magrittr", "stringr", "shiny", "xtable", "testthat", 
+    "scales", "profr", "mclust", "reshape2", "viridis", "httr", "rstan", "rstanarm",
+    "microbenchmark", "clustvarsel", "Rmixmod", "RColorBrewer", "RCurl",
+    "gtools", "forecast", "RcppArmadillo", "RcppEigen", "rsconnect",
+    "acs", "XML", "scrapeR", "xgboost", "imputeMulti", "synthACS"
+), dependencies = c("Depends", "Imports"))
 
 library(RCurl)
 library(httr)
 set_config( config( ssl_verifypeer = 0L ) )
 
-devtools::install_github("alexwhitworth/imputeMulti")
-devtools::install_github("alexwhitworth/glmEnsemble")
 devtools::install_github("alexwhitworth/emclustr")
-devtools::install_github("alexwhitworth/synthACS")
 
 Sys.setenv("PKG_CXXFLAGS"="-std=c++0x")
 devtools::install_github("alexwhitworth/imputation")
