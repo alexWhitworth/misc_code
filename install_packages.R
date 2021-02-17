3d61fcf498d0ed42f119a7c9d290f8b874f797cf
Alex Whitworth <alexWhitworth@users.noreply.github.com>
2021-02-17 07:20:52 -0800

    Update install_packages.R
    modify packages

--- install_packages.R
+++ install_packages.R
@@ -1,12 +1,13 @@
 install.packages(c(
     "lme4", "HLMdiag", "devtools", "merTools",  "dplyr", "data.table", "Rcpp", "RcppArmadillo", 
-    "RcppEigen", "ggplot2", "lubridate", "ggvis", "magrittr", "stringr", "shiny", "reshape2",
-    "quantmod", "zoo", "xts", "nnet", "forecast", "astsa", "bspec", "spectral", "prophet", 
-    "roxygen2", "foreign", "leaflet", "xtable", "testthat", 
+    "RcppEigen", "ggplot2", "lubridate", "ggvis", "magrittr", "stringr", 
+    "quantmod", "zoo", "xts", "forecast", "astsa", "bspec", "spectral", "prophet", 
+    "roxygen2", "foreign", "leaflet", "xtable", "testthat", "knitr", "gridExtra",
     "scales", "profr", "mclust", "viridis", "httr", "rstan", "rstanarm", "brms",
     "microbenchmark", "clustvarsel", "Rmixmod", "RColorBrewer", "RCurl",
     "gtools", "rsconnect", "Hmisc", "optimx", "lbfgs", "quadprog", "kernlab",
-    "acs", "XML", "scrapeR", "xgboost", "imputeMulti", "synthACS", "fredr"
+    "acs", "XML", "scrapeR", "xgboost", "imputeMulti", "synthACS", "fredr",
+    "ranger", "interpret", "bartMachine", "BART", "dbarts"
 ), dependencies = c("Depends", "Imports"))
 
 library(RCurl)
